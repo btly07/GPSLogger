5e34c6c17dc3996a16ed347368e7c95a7354b17e
btly07 <btly07@gmail.com>
2025-11-28 14:33:16 +0900

    20251003

--- SharedState.kt
+++ SharedState.kt
@@ -4,7 +4,7 @@ object SharedState {
     var latestLocationText: String = "位置情報未取得"
     var latestSatelliteText: String = "衛星情報未取得"
     var gpsLoggingEnabled: Boolean = true // ✅ 初期状態はON
-    var currentLowPowerMode: Boolean = false
+    var currentLowPowerMode: Boolean = true
 
     const val SAVE_INTERVAL_MS = 10_000L
     const val LOCATION_GAP_THRESHOLD_MS = 10 * 60 * 1000L // 10分
